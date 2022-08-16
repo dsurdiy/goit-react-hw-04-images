97cd8254e9b8a74c56d60a4ca96b088c3db98541
DimaSurdiy <dsurdiy@ukr.net>
2022-08-16 14:05:31 +0300

    App updated with hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,67 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
-import { ToastContainer } from 'react-toastify';
+import { Button } from 'components/Button/Button';
+import { Loader } from 'components/Loader/Loader';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import css from './App.module.css';
+import imagesAPI from 'services/images-api';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSubmit = searchQuery => {
+    setQuery(searchQuery);
+    setPage(1);
+    setImages([]);
   };
 
-  handleSubmit = values => {
-    this.setState({ searchQuery: values.searchQuery });
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    imagesAPI
+      .getImages(query, page)
+      .then(response => {
+        const newImages = response.data.hits;
+
+        if (newImages.length === 0) {
+          toast.warn('Oops, there are no images :(');
+        }
+
+        setImages(prevImages => [...prevImages, ...newImages]);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [query, page]);
+
+  const loadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { searchQuery } = this.state;
+  return (
+    <div className={css.container}>
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageGallery images={images} />
 
-    return (
-      <div className={css.container}>
-        <SearchBar onSubmit={this.handleSubmit} />
-        <ImageGallery query={searchQuery} />
+      {isLoading ? <Loader /> : null}
 
-        <ToastContainer />
-      </div>
-    );
-  }
-}
+      {images.length === 0 ? null : <Button onLoadMore={loadMore} />}
+
+      {error && <h2>Oops... something went wrong :( Try reloading the page</h2>}
+
+      <ToastContainer />
+    </div>
+  );
+};
