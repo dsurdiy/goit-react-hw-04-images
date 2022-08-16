6f426d215ee9eabd15ef998e3964f4ea2b4b2d5c
DimaSurdiy <dsurdiy@ukr.net>
2022-08-16 14:04:11 +0300

    images-api updated

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -3,36 +3,21 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '25683514-2bb572422bae18aceebfa826b';
 
-export default class ImagesApiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
+function getImages(query, page) {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    page,
+    per_page: 12,
+  });
 
-  getImages = async () => {
-    const searchParams = new URLSearchParams({
-      key: API_KEY,
-      q: this.searchQuery,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      page: this.page,
-      per_page: 12,
-    });
-
-    const response = await axios.get(`/?${searchParams}`);
-    this.page += 1;
-    return response.data.hits;
-  };
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-
-  resetPage = () => {
-    this.page = 1;
-  };
+  return axios.get(`/?${searchParams}`);
 }
+
+const api = {
+  getImages,
+};
+
+export default api;
